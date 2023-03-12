ce9a20b9e5e9fae1d37bc8edf8cb589334eecd7b
Muhammad Daniyal Mehmood <43728164+daniyalmehmood@users.noreply.github.com>
2023-03-12 15:29:59 +0400

    Inital commit

---
+++ src/main/java/com/example/demo/Model/TeacherDetail.java
@@ -0,0 +1,30 @@
+package com.example.demo.Model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Data
+@Table(name = "Teacher_Detail_Table")
+public class TeacherDetail {
+
+
+    @Id
+    Integer id;
+
+    @Column(name = "teacher_phone_number")
+    String phoneNumber;
+
+    @Column(name = "teacher_address")
+    String address;
+
+    @OneToOne
+    Teacher teacher;
+
+
+}
